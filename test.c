3c64d8b45feb82821b7c417f5d537e7c2014cbac
zzz6093 <35140301+zzz6093@users.noreply.github.com>
2018-11-12 13:12:30 +0900

    Update test.c

--- test.c
+++ test.c
@@ -27,12 +27,25 @@ int printnode();      //메뉴출력
 void createfile(FILE **, char *);   //변수에 저장된 값으로 파일생성
 void insertnode(Tree *, char *);    //트리에 값 설정
 void levelorder(Tree *);            //현재 트리를 레벨단위로 출력
+//case 2 사원등록에 필요한 함수
+DATA *pinsert();
+int checknode(Tree *T,char *);
+int samesearch(Tree *T,char *);
+void Tsearch(Tree *, DATA *);
+void addsearch(Tree *, DATA *);
+void insearch(Tree *, DATA *);
 
 int main(){
-  Tree *T;  //트리변수 생성
-  DATA *data; //이중링크로 사용할 변수 생성
-  
-  int s; //s : switch 사용을 위한 변수
+  Tree *T;
+  Tree **ap;
+  DATA *data;
+  char *pos[]={"사장","전무","과장","차장","이사","주임","사원"};
+  FILE *ifp;
+  char sea[20];
+  int s,i=0,same=0, same1=0;
+  int count1; // count잘못 가져왔을 가능성 있음.
+  char nam[3]={};
+  char posi[20];
   
   while(1){
     s=printnode();  //메뉴를 출력하고 상요자가 선택한 값을 얻음
@@ -46,6 +59,25 @@ int main(){
             levelorder(T);
             printf("\n");
           break;
+          case 2:
+
+            if(strcmp(T->data,"")==0){
+               printf("트리 없음\n");
+               break;
+            }
+            data=createnode1();
+            data=pinsert();
+            same=samesearch(T, data->num);
+            if(same!=1){
+               Tsearch(T, data);
+               searchfile(T, ifp);
+               printf("성공\n");
+            }
+            else{
+               printf("번호중복\n");
+            }
+            printsearch(T);
+         break;
     }
   }
   return 0;
@@ -182,4 +214,136 @@ void levelorder(Tree *T){
       sc++;
    }
 }
+
+DATA *pinsert(){
+   DATA *data;
+
+   data=createnode1();
+   printf("사원명 : ");
+   __fpurge(stdin);
+   gets(data->name);
+//   scanf("%s", data->name);
+   printf("전화번호 : ");
+   __fpurge(stdin);
+   scanf("%s", data->num);
+   printf("직책 : ");
+   __fpurge(stdin);
+   scanf("%s", data->cls);
+   printf("부서 : ");
+   __fpurge(stdin);
+   scanf("%s", data->cas);
+
+   return data;
+}
+
+int checknode(Tree *T,char *data){
+   Tree *p;
+   DATA *q;
+
+   p=T;
+   q=p->inside;
+   if(q->rlink!=NULL){
+      do{
+         q=q->rlink;
+         if(strcmp(q->num,data)==0){printf("\n");return 1;}
+      }while(p->inside->rlink!=q->rlink);
+   }
+   return 0;
+}
+
+int samesearch(Tree *T,char *data){
+   int count;
+   if(T){
+      return count=checknode(T,data)+samesearch(T->left,data)+samesearch(T->right,data);
+   }
+}
+
+//오류 날 가능성 많음
+void Tsearch(Tree *T, DATA *data){
+    Tree *p;
+    DATA *q, *r;
+    int ft=0;
+
+    p=T;
+    q=data;
+    printf("%s\n", q->cls);
+   
+    if(strcmp(p->data,"")==0){
+       printf("no value\n");
+    }
+    else{
+       while(p){
+          if(strcmp(p->data,q->cls)==0){
+             if(p->inside->rlink==NULL){
+                  p->inside->rlink=q;
+                  q->rlink=p->inside->rlink;
+                  q->llink=p->inside->rlink; 
+                  printf("성공\n");
+             }
+             else{
+               if(strcmp(p->inside->rlink->llink->name,q->name)<0){
+                  addsearch(p,data);
+               }
+               else{
+                  insearch(p,data);
+               }
+             }
+             break;
+          }
+          else if(strcmp(p->data,q->cls)<0){
+             p=p->right; 
+          }
+          else if(strcmp(p->data,q->cls)>0){
+             p=p->left;
+          }
+          else{
+             printf("novalue\n");
+             break;
+          }
+       }
+    }
+}
+
+void addsearch(Tree *T, DATA *data){
+   Tree *p;
+   DATA *q;
+
+   p=T;
+   q=data;
+   
+   p->inside->rlink->llink->rlink=q;
+   q->llink=p->inside->rlink->llink;
+   q->rlink=p->inside->rlink;
+   p->inside->rlink->llink=q;
+
+}
+
+void insearch(Tree *T, DATA *data){
+   Tree *p;
+   DATA *q, *r;
+
+   p=T;
+   q=data;
+   r=p->inside; 
+
+   if(strcmp(p->inside->rlink->name,q->name)>0){
+      q->llink=p->inside->rlink->llink;
+      p->inside->rlink->llink->rlink=q;
+      q->rlink=p->inside->rlink;
+      p->inside->rlink->llink=q;
+      p->inside->rlink=q;
+   }
+   else{
+      do{
+         r=r->rlink;
+         if(strcmp(r->rlink->name,q->name)>0){break;}
+      }while(r->rlink!=p->inside->rlink);
+      q->rlink=r->rlink;
+      r->rlink->llink=q;
+      r->rlink=q;
+      q->llink=r;
+   }
+}
+
+
 
